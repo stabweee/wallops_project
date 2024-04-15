3a9f3f7bbb5518e0b396712001142cd9a5ce1f98
stabweee <158535386+stabweee@users.noreply.github.com>
2024-04-14 22:24:11 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ module.exports = {
         body: ['Poppins'],
       },
 
-      //backgroundImage: {
-      //  'parallax': 'url("../public/img/test_bg_img.jpg")',
-      //}
+      backgroundImage: {
+       'parallax': 'url("../public/img/test_bg_img.jpg")',
+      }
     },
   },
   plugins: [],
