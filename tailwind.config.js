b17edc2deae09816268d6202f3059a121e40788e
stabweee <158535386+stabweee@users.noreply.github.com>
2024-04-14 20:41:02 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ['Platypi'],
+        body: ['Poppins'],
+      },
+
+      backgroundImage: {
+        'parallax': 'url("../public/img/test_bg_img.jpg")',
+      }
+    },
+  },
+  plugins: [],
+}
+
