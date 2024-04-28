ddd027934ad8c43b1d6d738134a419be57fc15c0
stabweee <158535386+stabweee@users.noreply.github.com>
2024-04-27 22:40:00 -0400

    update!!

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger)
+
 const t = gsap.timeline()
 const split = new SplitType("#title")
 
@@ -5,10 +7,92 @@ gsap.from("#title", {
     opacity: 0,
     y: 100,
     duration: 1,
-})
+});
 gsap.from(".char", {
     y: 100,
     stagger: {amount: 0.75},
     delay: 0.2,
     opacity: 0,
+});
+
+const body = document.getElementById('body');
+
+gsap.to(body, {
+    y: -window.innerHeight,
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: '#slide1',
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+    }
+});
+gsap.to(body, {
+    y: -window.innerHeight,
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: '#slide2',
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+    }
+});
+gsap.to(body, {
+    y: -window.innerHeight,
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: '#slide3',
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+    }
+});
+gsap.to(body, {
+    y: -window.innerHeight,
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: '#slide4',
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+    }
+});
+gsap.to(body, {
+    y: -window.innerHeight,
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: '#slide5',
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+    }
+});
+
+// slides.forEach((slide) => {
+//     const tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: slide,
+//             start: "top bottom",
+//             end: "top top",
+//             scrub: true,
+//         }
+//     });
+//     tl.to(body, {
+//         y: -window.innerHeight,
+//         ease: "power3.out",
+//     });
+// })
+
+// smooth scroll
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
 })
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
